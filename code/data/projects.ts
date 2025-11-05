59de2b484ae36dabb54030314fc5effdc34bf2ff
elendraug <elendraug@users.noreply.github.com>
2025-11-05 04:12:54 +0000

    Add Hyena, update Kat, organize projects

--- code/data/projects.ts
+++ code/data/projects.ts
@@ -1,10 +1,14 @@
 export const PROJECTS = [
   "Volume 0 Kickstarter",
   "Volume 0",
-  // TODO: rename to "FujoWebDev Website"
-  "Website",
+  // TODO: rename to "Websites"
+  // TODO: must be changed for all relevant team member .yaml files
+  // TODO: fujoweb.dev, fujocoded.com, etc. as sub-parts
+  "Websites",
   "Volume 0 Issue 1",
   "Fandom Cookies",
+  "FujoCoded",
+  "Test Project Name",
 ];
 
 export const VOLUME_0_ISSUE_1_ROLES = [
@@ -21,8 +25,36 @@ export const VOLUME_0_ISSUE_1_ROLES = [
   "Data Collection & Entry",
 ];
 
+export const WEBSITES_ROLES = [
+  "fujoweb.dev",
+  "fujocoded.com",
+];
+
+export const FUJOCODED_ROLES = [
+  "HimeOps",
+  "FujoCoded",
+];
+
+export const FANDOM_COOKIES_ROLES = [
+  "Artist",
+  "Extra",
+  "Programming",
+  "Digital Item Pack Assembly",
+  "Additional Research, Feedback, Development, and Assistance",
+  "Digital Item Pack Wallpapers"
+];
+
+export const TEST_PROJECTNAME_ROLES = [
+  "Role",
+  "Role",
+];
+
 export const PROJECT_ROLES = {
   "Volume 0 Issue 1": VOLUME_0_ISSUE_1_ROLES,
+  "Test Project Name": TEST_PROJECTNAME_ROLES,
+  "Websites": WEBSITES_ROLES,
+  "Fandom Cookies": FANDOM_COOKIES_ROLES,
+  "FujoCoded": FUJOCODED_ROLES,
 };
 
 export default PROJECTS;
